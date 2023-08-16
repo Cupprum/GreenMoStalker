dd7442bc8414d5e817b5e7d124743b8dc6b155c1
Samuel Branisa <branisa.samuel@icloud.com>
2023-08-16 11:04:33 +0000

    chore: formatting

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -5,5 +5,5 @@ import { GreenMobility } from '../lib/greenmobility-stack'
 
 const app = new cdk.App();
 new GreenMobility(app, 'GreenMobility', {
-  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+    env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
 });
--- cdk/lib/greenmobility-stack.ts
+++ cdk/lib/greenmobility-stack.ts
@@ -9,105 +9,105 @@ var cp = require('child_process');
 
 
 function packageLambdaCode(path: string): lambda.AssetCode {
-	return lambda.Code.fromAsset(path, {
-		bundling: {
-			image: lambda.Runtime.NODEJS_18_X.bundlingImage,
-			command: [],
-			local: {
-				tryBundle(outputDir: string) {
-					cp.execSync(
-						`
-							tsc --target es6 --moduleResolution node --outDir ${outputDir} ${path}/index.ts
-							cp ${path}/package.json ${outputDir}
-							cp ${path}/package-lock.json ${outputDir}
-							cd ${outputDir}
-							npm install
-						`,
-						{ stdio: 'inherit' }
-					);
-					return true;
-				},
-			},
-		}
-	});
+    return lambda.Code.fromAsset(path, {
+        bundling: {
+            image: lambda.Runtime.NODEJS_18_X.bundlingImage,
+            command: [],
+            local: {
+                tryBundle(outputDir: string) {
+                    cp.execSync(
+                        `
+                            tsc --target es6 --moduleResolution node --outDir ${outputDir} ${path}/index.ts
+                            cp ${path}/package.json ${outputDir}
+                            cp ${path}/package-lock.json ${outputDir}
+                            cd ${outputDir}
+                            npm install
+                        `,
+                        { stdio: 'inherit' }
+                    );
+                    return true;
+                },
+            },
+        }
+    });
 }
 
 export class GreenMobility extends cdk.Stack {
-	constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-		super(scope, id, props);
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
 
-		const greenmoApi = new GreenMoApi(this);
+        const greenmoApi = new GreenMoApi(this);
 
-		const chargableCarsLambda = this.chargableCarsLambda();
+        const chargableCarsLambda = this.chargableCarsLambda();
 
-		greenmoApi.addLambda(chargableCarsLambda, 'GET', 'cars');
-	}
+        greenmoApi.addLambda(chargableCarsLambda, 'GET', 'cars');
+    }
 
-	private chargableCarsLambda(): lambda.Function {
-		// Package the code.
-		const codePath = path.join(__dirname, '..', 'lambda', 'chargableCars');
-		const code = packageLambdaCode(codePath);
+    private chargableCarsLambda(): lambda.Function {
+        // Package the code.
+        const codePath = path.join(__dirname, '..', 'lambda', 'chargableCars');
+        const code = packageLambdaCode(codePath);
 
-		const func = new lambda.Function(this, 'chargableCarsLambda', {
-			functionName: 'chargableCarsLambda',
-			code: code,
-			handler: 'index.handler',
-			runtime: lambda.Runtime.NODEJS_18_X,
-			timeout: cdk.Duration.seconds(15),
-		});
+        const func = new lambda.Function(this, 'chargableCarsLambda', {
+            functionName: 'chargableCarsLambda',
+            code: code,
+            handler: 'index.handler',
+            runtime: lambda.Runtime.NODEJS_18_X,
+            timeout: cdk.Duration.seconds(15),
+        });
 
-		// Allow accessing specific SSM Parameters.
-		func.addToRolePolicy(
-			new iam.PolicyStatement({
-				actions: ['ssm:GetParameter'],
-				resources: [`arn:aws:ssm:${this.region}:${this.account}:parameter/greenmo/*`],
-			})
-		);
+        // Allow accessing specific SSM Parameters.
+        func.addToRolePolicy(
+            new iam.PolicyStatement({
+                actions: ['ssm:GetParameter'],
+                resources: [`arn:aws:ssm:${this.region}:${this.account}:parameter/greenmo/*`],
+            })
+        );
 
-		// Allow invocation from apigateway.
-		func.addPermission('ApiGatewayInvokePermission', {
-			principal: new cdk.aws_iam.ServicePrincipal('apigateway.amazonaws.com'),
-			action: 'lambda:InvokeFunction',
-		});
+        // Allow invocation from apigateway.
+        func.addPermission('ApiGatewayInvokePermission', {
+            principal: new cdk.aws_iam.ServicePrincipal('apigateway.amazonaws.com'),
+            action: 'lambda:InvokeFunction',
+        });
 
-		// Define required parameters.
-		new ssm.StringParameter(this, 'mapsApiToken', {
-			parameterName: '/greenmo/mapsApiToken',
-			stringValue: process.env.OPEN_MAPS_API_TOKEN ?? '',
-		});
+        // Define required parameters.
+        new ssm.StringParameter(this, 'mapsApiToken', {
+            parameterName: '/greenmo/mapsApiToken',
+            stringValue: process.env.OPEN_MAPS_API_TOKEN ?? '',
+        });
 
-		return func;
-	}
+        return func;
+    }
 }
 
 class GreenMoApi extends apigw.RestApi {
-	constructor(scope: Construct) {
-		super(scope, 'greenMoApi', {
-			restApiName: 'greenmoApi',
-			apiKeySourceType: apigw.ApiKeySourceType.HEADER,
-			binaryMediaTypes: ["*/*"]
-		});
+    constructor(scope: Construct) {
+        super(scope, 'greenMoApi', {
+            restApiName: 'greenmoApi',
+            apiKeySourceType: apigw.ApiKeySourceType.HEADER,
+            binaryMediaTypes: ["*/*"]
+        });
 
-		// Hide the lambda functions behind apiKey.
-		// Currently useful because i don't want to go over google maps free tier.
-		const apiKey = this.addApiKey('apiKey', {
-			value: process.env.GREENMO_API_KEY ?? '',
-		});
+        // Hide the lambda functions behind apiKey.
+        // Currently useful because i don't want to go over google maps free tier.
+        const apiKey = this.addApiKey('apiKey', {
+            value: process.env.GREENMO_API_KEY ?? '',
+        });
 
-		const usagePlan = this.addUsagePlan('usagePlan');
-		
-		// Usage plan enforces the use of apiKey.
-		usagePlan.addApiKey(apiKey);
+        const usagePlan = this.addUsagePlan('usagePlan');
 
-		// Usage plan has to be bound to a specific stage in order to work.
-		usagePlan.addApiStage({ stage: this.deploymentStage });
-	}
+        // Usage plan enforces the use of apiKey.
+        usagePlan.addApiKey(apiKey);
 
-	// Wrapper function to add lambda to apigateway path.
-	public addLambda(func: lambda.Function, method: string, path: string) {
-		const integration = new apigw.LambdaIntegration(func);
+        // Usage plan has to be bound to a specific stage in order to work.
+        usagePlan.addApiStage({ stage: this.deploymentStage });
+    }
 
-		const route = this.root.addResource(path);
-		route.addMethod(method, integration, { apiKeyRequired: true });
-	}
+    // Wrapper function to add lambda to apigateway path.
+    public addLambda(func: lambda.Function, method: string, path: string) {
+        const integration = new apigw.LambdaIntegration(func);
+
+        const route = this.root.addResource(path);
+        route.addMethod(method, integration, { apiKeyRequired: true });
+    }
 }
