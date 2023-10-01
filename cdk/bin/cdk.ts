2a616d15ad2e84305332649857fdb3f6594662dc
Samuel Branisa <branisa.samuel@icloud.com>
2023-10-01 17:27:49 +0000

    build: fix accessing variables

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -4,9 +4,6 @@ import * as cdk from 'aws-cdk-lib';
 import { GreenMobility } from '../lib/greenmobility-stack';
 
 const app = new cdk.App();
-console.log('-------test123');
-console.log(process.env.CDK_DEFAULT_ACCOUNT);
-console.log(process.env.CDK_DEFAULT_REGION);
 new GreenMobility(app, 'GreenMobility', {
     env: {
         account: process.env.CDK_DEFAULT_ACCOUNT,
