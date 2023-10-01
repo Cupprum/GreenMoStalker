0d4742efbd341918b1feea3c71481463ded2da2b
Samuel Branisa <branisa.samuel@icloud.com>
2023-10-01 17:15:55 +0000

    chore: debug failing deployment

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -4,6 +4,9 @@ import * as cdk from 'aws-cdk-lib';
 import { GreenMobility } from '../lib/greenmobility-stack';
 
 const app = new cdk.App();
+console.log('-------test123');
+console.log(process.env.CDK_DEFAULT_ACCOUNT);
+console.log(process.env.CDK_DEFAULT_REGION);
 new GreenMobility(app, 'GreenMobility', {
     env: {
         account: process.env.CDK_DEFAULT_ACCOUNT,
